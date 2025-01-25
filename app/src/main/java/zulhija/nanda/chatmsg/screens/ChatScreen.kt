6554739f08159ede1f97508e867bea3b376b9ed3
Uda Jul <superdiazzz@gmail.com>
2025-01-25 23:06:22 +0700

    clean UIs

--- app/src/main/java/zulhija/nanda/chatmsg/screens/ChatScreen.kt
+++ app/src/main/java/zulhija/nanda/chatmsg/screens/ChatScreen.kt
@@ -1,8 +1,6 @@
 package zulhija.nanda.chatmsg.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,19 +9,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,7 +24,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -42,7 +34,6 @@ import androidx.compose.ui.unit.sp
 import zulhija.nanda.chatmsg.Constants
 import zulhija.nanda.chatmsg.Constants.Color3
 import zulhija.nanda.chatmsg.data.Message
-import zulhija.nanda.chatmsg.viewmodels.ChatViewModel
 
 @Composable
 fun ChatScreen(
@@ -76,36 +67,37 @@ fun ChatScreen(
             messages.forEach { message ->
 
                 if (message.sender == "left") {
-                    Column {
+                    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = if (title == "Left Chat") Alignment.End else Alignment.Start) {
                         Text(text = message.sender,
                             style = TextStyle(fontWeight = FontWeight.Bold),
-                            textAlign = if (title == "Left Chat") TextAlign.End else TextAlign.Start,
-                            modifier = Modifier.fillMaxWidth())
+                            textAlign = if (title == "Left Chat") TextAlign.End else TextAlign.Start,)
+
+                        Spacer(modifier = Modifier.height(8.dp))
                         Text(
                             textAlign = if (title == "Left Chat") TextAlign.End else TextAlign.Start,
                             text = message.content,
                             style = MaterialTheme.typography.bodyMedium,
                             modifier = Modifier
-                                .fillMaxWidth()
+                                .background(Color.White)
                                 .padding(8.dp)
+
                         )
                     }
-
                 }
                 else {
-                    Column {
+                    Column (modifier = Modifier.fillMaxWidth(), horizontalAlignment = if (title == "Left Chat") Alignment.Start else Alignment.End){
                         Text(
                             textAlign = if (title == "Left Chat") TextAlign.Start else TextAlign.End,
                             text = message.sender,
-                            modifier = Modifier.fillMaxWidth(),
                             style = TextStyle(fontWeight = FontWeight.Bold)
                         )
+                        Spacer(modifier = Modifier.height(8.dp))
                         Text(
                             textAlign = if (title == "Left Chat") TextAlign.Start else TextAlign.End,
                             text = message.content,
                             style = MaterialTheme.typography.bodyMedium,
                             modifier = Modifier
-                                .fillMaxWidth()
+                                .background(Color.White)
                                 .padding(8.dp)
                         )
                     }
